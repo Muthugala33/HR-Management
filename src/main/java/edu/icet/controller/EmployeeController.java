eedb98259ef936269f20305f7f1b6308de4751b0
Pasindu keshan <pasikeshan21.com>
2025-04-29 10:33:03 +0530

    create layer architecture & start a first crud operation(addEmployee)

---
+++ src/main/java/edu/icet/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package edu.icet.controller;
+
+import edu.icet.dto.Employee;
+import edu.icet.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employee")
+@RequiredArgsConstructor
+
+public class EmployeeController {
+
+    final EmployeeService service;
+
+    @PostMapping("add")
+    public void addEmployee(@RequestBody Employee employee){
+        service.addEmployee(employee);
+    }
+
+}
---
+++ src/main/java/edu/icet/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,23 @@
+package edu.icet.service.impl;
+
+import edu.icet.dto.Employee;
+import edu.icet.entity.EmployeeEntity;
+import edu.icet.repository.EmployeeRepository;
+import edu.icet.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    final EmployeeRepository repository;
+    final ModelMapper mapper;
+    @Override
+    public void addEmployee(Employee employee){
+        repository.save(mapper.map(employee, EmployeeEntity.class));
+
+
+    }
+}
